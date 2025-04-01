ff832a9201e82d711501167a1fdb2f876bc74083
Aayush Chaudhary <aayushcyy@gmail.com>
2025-04-01 15:42:44 +0530

    updated routes file

--- src/routes/slack.route.js
+++ src/routes/slack.route.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { slackEventController } from "../controllers/slack.controllers.js";
+import {
+  slackEventController,
+  handleSlashCommand,
+  handleAction,
+} from "../controllers/slack.controllers.js";
 import { verifySlackRequest } from "../middlewares/verifySlackRequest.js";
 
 const router = express.Router();
 
 router.post("/", slackEventController);
 router.post("/api/slack/command", verifySlackRequest, handleSlashCommand);
-router.post("/api/slack/actions", verifySlackRequest, handleActions);
-router.post("/api/slack/events", verifySlackRequest, handleEvent);
+router.post("/api/slack/actions", verifySlackRequest, handleAction);
+// router.post("/api/slack/events", verifySlackRequest, handleEvent);
 
 export default router;
