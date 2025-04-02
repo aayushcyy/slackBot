2434d75b31c753e9872327d00c88a55d14c69a00
Aayush Chaudhary <aayushcyy@gmail.com>
2025-04-02 19:02:44 +0530

    remove middlware for development

--- src/routes/slack.route.js
+++ src/routes/slack.route.js
@@ -9,7 +9,8 @@ import { verifySlackRequest } from "../middlewares/verifySlackRequest.js";
 const router = express.Router();
 
 router.post("/", slackEventController);
-router.post("/api/slack/command", verifySlackRequest, handleSlashCommand);
-router.post("/api/slack/actions", verifySlackRequest, handleAction);
+// add the middleware(verifySlackRequest) in below both routes
+router.post("/api/slack/command", handleSlashCommand);
+router.post("/api/slack/actions", handleAction);
 
 export default router;
