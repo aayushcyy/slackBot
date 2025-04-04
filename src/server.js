00359c4d29ffd863cca652a9e8bfd8d699909224
Aayush Chaudhary <aayushcyy@gmail.com>
2025-04-04 14:01:35 +0530

    successfully commented server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 import express from "express";
-import "dotenv/config"; // imports and configs dotenv
-import bodyParser from "body-parser";
+import "dotenv/config"; // loads env variable from .env
 import slackEventRouter from "./routes/slack.route.js";
 import cors from "cors";
 
@@ -9,7 +8,7 @@ const app = express();
 // enable CORS for all origins
 app.use(cors({ origin: "*", credentials: true }));
 
-// parse JSON requests for all routes
+// parse incoming JSON requests for all routes
 app.use(express.json());
 
 // Use express.raw() for Slack routes(middlewares) to capture the raw body
@@ -22,23 +21,14 @@ app.use(
   express.raw({ type: "application/x-www-form-urlencoded" })
 );
 
-// Parse the body as URL-encoded after the raw middleware for use in controllers
-//remove this just for check as it doesn't needed
-// app.use("/api/slack/command", express.urlencoded({ extended: true }));
-// app.use("/api/slack/actions", express.urlencoded({ extended: true }));
-
-// Development log middleware
-app.use((req, res, next) => {
-  console.log("Received request:", req.method, req.url, req.headers);
-  next();
-});
-
 // Slack event subscription route
 app.use("/", slackEventRouter);
 
+// health check route
 app.get("/", (req, res) => {
   res.send("Welcome to the Home Page!");
 });
 
+// starting server on specified port
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
